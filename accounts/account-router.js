e7cb80c1459782a212a40d015bfdae5b4d4a24aa
sofia Luna <Sofia21luna@gmail.com>
2020-11-02 15:09:11 -0600

    gets all the accounts

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -2,6 +2,27 @@ const express = require('express');
 const db = require("../data/dbConfig.js");
 const router = express.Router();
 
+const Accounts = {
+
+    getAll() {
+        return db('accounts')
+    },
+
+    getById(id) {
+
+    }
+}
+
+router.get('/', (req, res) => {
+    Accounts.getAll()
+    .then(allAccounts => {
+        res.status(200).json(allAccounts)
+    })
+    .catch(error => {
+        // res.json({ message: 'oops, something went wrong' }) // production
+        res.json({ error: error.message }) // development
+    })
+})
 
 
 module.exports = router;
